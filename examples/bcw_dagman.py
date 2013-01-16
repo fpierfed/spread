2bbe62e519e44876b43923d5fe4a43ed0e55af57
Francesco Pierfederici <fpierfed@gmail.com>
2013-01-16 17:35:33 -0500

    added debugging timing info.

--- examples/bcw_dagman.py
+++ examples/bcw_dagman.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 import os
 import sys
+import time
 
-from client import async_call
+from spreader.client import async_call
+
 
 
 
@@ -15,61 +17,97 @@ DATASET = 'raw-000001'
 NUM_CCDS = 4
 
 
-print('Running PROC_MEF')
-defer = async_call('system',
-                   [PROC_MEF,
-                    '-i',
-                    os.path.join(ROOT, 'data/dataset_001/%s.fits' % (DATASET)),
-                    '-o',
-                    '/tmp/%s_' % (DATASET) + '%(ccdId)s.fits'])
-while(not defer.is_ready()):
-    defer.wait()
-res = defer.result()
-if(res['terminated'] or res['exit_code'] != 0):
-    print(res)
-    sys.exit(res['exit_code'])
 
-results = []
-defers = []
-err = 0
-for _id in range(NUM_CCDS):
-    print('Running PROC_SIF')
+def run(verbose=False):
+    if(verbose):
+        print('Enqueueing PROC_MEF (time: %f)' % (time.time()))
     defer = async_call('system',
-                       [PROC_SIF,
+                       [PROC_MEF,
                         '-i',
-                        '/tmp/%s_%d.fits' % (DATASET, _id),
+                        os.path.join(ROOT,
+                                     'data/dataset_001/%s.fits' % (DATASET)),
                         '-o',
-                        '/tmp/%s_calib_%d.fits' % (DATASET, _id)])
-    defers.append(defer)
+                        '/tmp/%s_' % (DATASET) + '%(ccdId)s.fits'])
+    if(verbose):
+        print('Enqueued %f' % (time.time()))
 
-while(len(results) < NUM_CCDS):
+    while(not defer.is_ready()):
+        defer.wait()
+    res = defer.result()
+    if(verbose):
+        print(res)
+    if(res['terminated'] or res['exit_code'] != 0):
+        return(res['exit_code'])
+
+    results = {}
+    defers = []
+    err = 0
     for _id in range(NUM_CCDS):
-        if(not defers[_id].is_ready()):
-            defers[_id].wait()
-            continue
+        if(verbose):
+            print('Enqueueing PROC_SIF (time: %f)' % (time.time()))
+        defer = async_call('system',
+                           [PROC_SIF,
+                            '-i',
+                            '/tmp/%s_%d.fits' % (DATASET, _id),
+                            '-o',
+                            '/tmp/%s_calib_%d.fits' % (DATASET, _id)])
+        if(verbose):
+            print('Enqueued %f' % (time.time()))
+        defers.append(defer)
 
-        res = defers[_id].result()
-        results.append((_id, res))
-        if(res['terminated'] or res['exit_code'] != 0):
-            print(_id, res)
-            err = res['exit_code']
-if(err):
-    print(results)
+    while(len(results.keys()) < NUM_CCDS):
+        for _id in range(NUM_CCDS):
+            if(not defers[_id].is_ready()):
+                defers[_id].wait()
+                continue
+
+            res = defers[_id].result()
+            results[_id] = res
+            if(res['terminated'] or res['exit_code'] != 0):
+                err = res['exit_code']
+    if(verbose):
+        print(results)
+    if(err):
+        return(err)
+
+    if(verbose):
+        print('Enqueueing FINISH_MEF (time: %f)' % (time.time()))
+    defer = async_call('system',
+                       [FINISH_MEF,
+                        '-i',
+                         '/tmp/%s_calib_' % (DATASET) + '%(ccdId)s.fits',
+                        '-o',
+                        '/tmp/%s_calib.fits' % (DATASET),
+                        '-n',
+                        NUM_CCDS])
+    if(verbose):
+        print('Enqueued %f' % (time.time()))
+
+    while(not defer.is_ready()):
+        defer.wait()
+    res = defer.result()
+    if(verbose):
+        print(res)
+    if(res['terminated'] or res['exit_code'] != 0):
+        return(res['exit_code'])
+    return(0)
+
+
+
+
+if(__name__ == '__main__'):
+    verbose = False
+    if(len(sys.argv) == 2 and sys.argv[1] == '-v'):
+        verbose = True
+
+    t0 = time.time()
+    err = run(verbose)
+    t1 = time.time()
+
+    if(verbose):
+        print('Started:  %f' % (t0))
+        print('Ended:    %f' % (t1))
+        print('Duration: %f' % (t1 - t0))
+
     sys.exit(err)
-
-print('Running FINISH_MEF')
-defer = async_call('system',
-                   [FINISH_MEF,
-                    '-i',
-                     '/tmp/%s_calib_' % (DATASET) + '%(ccdId)s.fits',
-                    '-o',
-                    '/tmp/%s_calib.fits' % (DATASET),
-                    '-n',
-                    NUM_CCDS])
-while(not defer.is_ready()):
-    defer.wait()
-res = defer.result()
-if(res['terminated'] or res['exit_code'] != 0):
-    print(res)
-    sys.exit(res['exit_code'])
 
