aaa2b9dbd61c48939f8ae9f61371e0e1fbfa3502
Francesco Pierfederici <fpierfed@gmail.com>
2013-01-16 17:28:05 -0500

    Implemented singleton pattern for RpcClient class to avoid init performance hit.

--- src/client.py
+++ src/client.py
@@ -11,8 +11,25 @@ import pika
 QUEUE_NAME = 'rpc_queue'
 
 
+class Singleton(type):
+    """
+    Singleton metaclass from http://stackoverflow.com/questions/31875/ \
+        is-there-a-simple-elegant-way-to-define-singletons-in-python/33201#33201
+    """
+    def __init__(cls, name, bases, dict):
+        super(Singleton, cls).__init__(name, bases, dict)
+        cls.instance = None
+
+    def __call__(cls,*args,**kw):
+        if cls.instance is None:
+            cls.instance = super(Singleton, cls).__call__(*args, **kw)
+        return cls.instance
+
+
+
 
 class RpcClient(object):
+    __metaclass__ = Singleton
     """
     Generic proxy for distributed PRC servers.
     """
